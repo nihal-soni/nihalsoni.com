e24774e271b8f162ed026dd4938156f23209f367
nihal-soni <nihalsoni0523@gmail.com>
2025-08-25 18:11:16 +0530

    refactor(container): enhance Container component with additional styling

--- src/components/common/container.tsx
+++ src/components/common/container.tsx
@@ -7,5 +7,14 @@ export const Container = ({
   children: React.ReactNode;
   className?: string;
 }) => {
-  return <div className={cn("mx-auto max-w-4xl", className)}>{children}</div>;
+  return (
+    <div
+      className={cn(
+        "container mx-auto max-w-4xl px-4 animate-fade-in-blur border",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
